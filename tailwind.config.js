ab82690ae77c8d958ec034df211020e4aef0d5a9
Christian Escobar <christian.escobar@autocred.cl>
2022-04-12 14:15:36 -0400

    Change config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,45 @@
 module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          900: '#141C52',
+          800: '#141c52e6',
+          700: '#141c52cc',
+          600: '#141c52b3',
+          500: '#141c5299',
+          400: '#141c5280',
+          300: '#141c5266',
+          200: '#141c524d',
+          100: '#141c5233',
+        },
+        secondary: {
+          900: '#d40f7d',
+          800: '#d40f7de6',
+          700: '#d40f7dcc',
+          600: '#d40f7db3',
+          500: '#d40f7d99',
+          400: '#d40f7d80',
+          300: '#d40f7d66',
+          200: '#d40f7d4d',
+          100: '#d40f7d33',
+        },
+        black: {
+          100: '#101921',
+          50: '#10192180',
+          25: '#10192140',
+        }
+      }
+    },
   },
   plugins: [],
+  content: [
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `composables/**/*.{js,ts}`,
+    `plugins/**/*.{js,ts}`,
+    `App.{js,ts,vue}`,
+    `app.{js,ts,vue}`
+  ]
 }
