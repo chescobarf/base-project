4456e70c8e90e9db970c0226d2c3bf7a15155018
Christian Escobar <christian.escobar@autocred.cl>
2022-04-12 14:16:50 -0400

    Add Brand Colors

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="flex flex-col w-full gap-2 md:flex-row">
+  <div
+    class="flex flex-col w-full gap-2 md:flex-row bg-primary-900 text-secondary-900 py-2 px-4"
+  >
     <NuxtLink to="/">Home page</NuxtLink>
-    <NuxtLink to="/about">About page</NuxtLink>
-    <NuxtLink to="/contact">Contact page</NuxtLink>
+    <NuxtLink to="/">About page</NuxtLink>
+    <NuxtLink to="/">Contact page</NuxtLink>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,9 @@
-import { NuxtLink } from '../.nuxt/components';
 <template>
-  <div class="container mx-auto bg-gray-300 p-8">
-    <h1 class="font-bold text-gray-600 text-lg">Hello, Tailwind 3!</h1>
+  <div class="container mx-auto bg-primary-900 p-8">
+    <h1 class="font-bold text-secondary-900 text-lg">Hello, Tailwind 3!</h1>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
