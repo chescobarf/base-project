0e3d9c7758bd1a08477161d33d4599ecef5bc3ca
Christian Escobar <christian.escobar@autocred.cl>
2022-04-13 11:04:24 -0400

    Fix height IFrame encuesta

--- pages/encuesta.vue
+++ pages/encuesta.vue
@@ -3,21 +3,20 @@ import { NuxtLink } from '../.nuxt/components';
   <div
     class="grid place-content-center bg-gradient-to-r from-primary-900 to-secondary-900"
   >
-    <img src="/img/autocred.png" alt="" class="justify-self-center" />
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSfewHTPdXUbussxATteTXvwTuNJDGn6Rhl0F0itasMIVtf-tA/viewform?embedded=true"
-      width="640"
-      height="2000"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
-      >Cargando…</iframe
-    >
     <img
       src="~/assets/img/autocred.png"
       alt="Logo Autocred"
       class="justify-self-center"
     />
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSfewHTPdXUbussxATteTXvwTuNJDGn6Rhl0F0itasMIVtf-tA/viewform?embedded=true"
+      width="640"
+      height="1500"
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+      >Cargando…</iframe
+    >
   </div>
 </template>
 
