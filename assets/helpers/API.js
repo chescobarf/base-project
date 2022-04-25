00784c098cf84e199b49e6202a1ecac046e47e6a
Christian Escobar <christian.escobar@autocred.cl>
2022-04-25 16:49:16 -0400

    Add Helper API

---
+++ assets/helpers/API.js
@@ -0,0 +1,25 @@
+export const brandsCars = [
+    { id: 1, name: "Chevrolet" },
+    { id: 2, name: "Nissan" },
+    { id: 3, name: "Suzuki" },
+    { id: 4, name: "Hyundai" },
+    { id: 5, name: "KIA" },
+    { id: 6, name: "Renault" },
+];
+
+export const models = [
+    { id: 1, name: "Chevrolet", models: [{ name: 'Sail' }, { name: 'Sonic' }, { name: 'Camaro' }] },
+    { id: 2, name: "Nissan", models: [{ name: 'Versa' }, { name: 'Qasqai' }, { name: 'Trail' }] },
+    { id: 3, name: "Suzuki", models: [{ name: 'Jimny' }, { name: 'Spresso' }, { name: 'Ignis' }] },
+    { id: 4, name: "Hyundai", models: [{ name: 'Accent' }, { name: 'Tucson' }, { name: 'i10' }] },
+    { id: 5, name: "KIA", models: [{ name: 'Sorento' }, { name: 'Morning' }, { name: 'Rio 5' }] },
+    { id: 6, name: "Renault", models: [{ name: 'Clio' }, { name: 'Kwid' }, { name: 'Duster' }] },
+];
+
+export const direcciones = [
+    { name: 'R.Metropolitana', direccion: 'Los Militares 5001, of 404, Las Condes', telefono: '+5692342423', correo: 'contacto@autocred.cl', },
+    { name: 'Curico', direccion: 'Direccion Curico 1000', telefono: '+5692342423', correo: 'contacto@autocred.cl', },
+    { name: 'Concepcion', direccion: 'Direccion Concepcion 1000', telefono: '+5692342423', correo: 'contacto@autocred.cl', },
+    { name: 'Temuco', direccion: 'Direccion Temuco 1000', telefono: '+5692342423', correo: 'contacto@autocred.cl', },
+    { name: 'Otras Regiones', direccion: 'Direccion Otras Regiones 1000', telefono: '+5692342423', correo: 'contacto@autocred.cl', },
+]
