ab271228c4a15c9a251826673c84ea47b16be69d
Christian Escobar <christian.escobar@autocred.cl>
2022-04-29 14:23:15 -0400

    Add Page Nosotros

---
+++ layouts/nosotros/NuestroRecorrido.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="mb-16">
+    <Heading1 content="Nuestro recorrido" />
+    <div class="grid grid-cols-2 pattern">
+      <img
+        src="https://api.lorem.space/image/car?w=300&h=300"
+        alt=""
+        class="image"
+      />
+      <div class="content">
+        <p class="font-medium text-primary-900 text-lg">
+          <span class="text-secondary-900 font-bold">Autocred</span> ipsum dolor
+          sit amet consectetur adipisicing elit. Sed, enim eveniet itaque facere
+          harum culpa atque dolorum exercitationem temporibus! Reprehenderit
+          vitae magni repudiandae voluptatum! Eaque totam veniam aut autem
+          itaque?
+        </p>
+        <p class="font-medium text-primary-900 text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet natus
+          facere nesciunt vero recusandae expedita deleniti, nulla voluptates,
+          optio alias veritatis vitae magnam similique totam, nobis eos voluptas
+          minima. Exercitationem.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Heading1 from "~~/components/Heading.vue";
+</script>
+
+<style scoped>
+.image {
+  border-bottom-right-radius: 100px;
+}
+.content {
+  padding: 3rem 3rem 3rem 0;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+.pattern {
+  padding: 0 0 4rem 0;
+  background-image: url("https://thumbs.dreamstime.com/b/light-grey-network-web-texture-seamless-pattern-great-space-inspired-wallpaper-backgrounds-invitations-packaging-design-132890319.jpg");
+}
+</style>
---
+++ pages/nosotros.vue
@@ -0,0 +1,51 @@
+<template>
+  <Container>
+    <BannerHome>
+      <template #content>
+        <Heading
+          content="Hacer un buen negocio es un camino en que muchos se pierden. Para llegar a destino, confia en un coopiloto experto"
+          headingType="h2"
+          class="mb-5"
+        />
+        <Heading
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+          headingType="h3"
+          class="mb-5"
+        />
+        <div>
+          <div class="flex flex-row gap-4">
+            <Button text="Viajemos juntos" class="secondary" />
+          </div>
+        </div>
+      </template>
+      <template #image>
+        <div class="grid place-content-center">
+          <img
+            src="https://picsum.photos/id/199/536/354"
+            alt="lorem"
+            class=""
+          />
+        </div>
+      </template>
+    </BannerHome>
+    <NuestroRecorrido />
+    <Servicios />
+    <Comentarios />
+    <Respaldo />
+    <Hablemos />
+  </Container>
+</template>
+
+<script setup>
+import Container from "~~/layouts/Container.vue";
+import Servicios from "~~/layouts/Servicios.vue";
+import BannerHome from "~~/layouts/BannerHome.vue";
+import Respaldo from "~~/layouts/Respaldo.vue";
+import Heading from "~~/components/Heading.vue";
+import Button from "~~/components/Button.vue";
+import Comentarios from "~~/layouts/Comentarios.vue";
+import Hablemos from "~~/layouts/Hablemos.vue";
+import NuestroRecorrido from "~~/layouts/nosotros/NuestroRecorrido.vue";
+</script>
+
+<style scoped></style>
