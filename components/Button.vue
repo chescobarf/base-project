94cd1ffe7993b1b8f271032069228dc0538861cd
Christian Escobar <christian.escobar@autocred.cl>
2022-05-02 18:01:15 -0400

    Fix Button based on styleguide

--- components/Button.vue
+++ components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="whitespace-nowrap inline-flex gap-1 items-center justify-center py-2 border border-transparent rounded-md shadow-sm text-base px-7 cursor-pointer"
+    class="whitespace-nowrap inline-flex gap-1 items-center justify-center py-2 border border-transparent rounded-lg shadow-sm text-lg px-5 cursor-pointer"
     :class="{
       'bg-secondary-900 hover:bg-secondary-700 text-white': secondary,
       'bg-primary-900 hover:bg-secondary-700 text-white': primary,
@@ -8,9 +8,10 @@
         outlinePrimary,
       'bg-transparent text-secondary-900 border-2 border-secondary-900 hover:bg-secondary-900 hover:text-white':
         outlineSecondary,
+      'py-1': user,
     }"
   >
-    <button class="font-medium">
+    <button class="font-semibold">
       {{ text }}
     </button>
     <UserIcon class="h-5 w-5" v-if="user" />
