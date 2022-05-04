6f1a9b485ab073af35c54f703168ea6c8fa120d8
Christian Escobar <christian.escobar@autocred.cl>
2022-05-04 14:24:29 -0400

    Add TesterAPI

---
+++ components/TesterApi.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex gap-1 flex-col">
+    <div v-for="(marca, index) in marcas.data" :key="index">
+      <p>{{ marca }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data: marcas } = await useFetch(
+  "http://dev.autocred.cl/api/utilities/vehicles/brands",
+  { pick: ["data"] }
+);
+</script>
+
+<style lang="scss" scoped></style>
