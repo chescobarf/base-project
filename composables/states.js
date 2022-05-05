4cbb99dff0cb58a9bb7573b3775df4a2052a0605
Christian Escobar <christian.escobar@autocred.cl>
2022-05-05 12:03:37 -0400

    Add States for Cotizacion

--- composables/states.js
+++ composables/states.js
@@ -1,1 +1,11 @@
+//Cotizacion
 export const useMarca = () => useState('Marca', () => 'Marca')
+export const useModelos = () => useState('Modelos', () => 'Modelos')
+export const useModelo = () => useState('Modelo', () => 'Modelo')
+export const useVersion = () => useState('Version', () => 'Version')
+export const useAnio = () => useState('Año', () => 'Año')
+export const useCredito = () => useState('Credito', () => "Credito")
+export const useValor = () => useState('Valor', () => 0)
+export const usePie = () => useState('Pie', () => 0)
+
+
