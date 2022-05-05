a90bb3e8cfc4266287ba4bc07fe2e3d950a18d3a
Christian Escobar <christian.escobar@autocred.cl>
2022-05-05 16:00:48 -0400

    Fix button styles

--- components/Button.vue
+++ components/Button.vue
@@ -3,7 +3,7 @@
     class="whitespace-nowrap inline-flex gap-1 items-center justify-center py-2 border border-transparent rounded-lg shadow-sm text-lg px-5 cursor-pointer"
     :class="{
       'bg-secondary-900 hover:bg-secondary-700 text-white': secondary,
-      'bg-primary-900 hover:bg-secondary-700 text-white': primary,
+      'bg-primary-900 hover:bg-primary-700 text-white': primary,
       'bg-transparent text-primary-900 border-2 border-primary-900 hover:bg-primary-900 hover:text-white':
         outlinePrimary,
       'bg-transparent text-secondary-900 border-2 border-secondary-900 hover:bg-secondary-900 hover:text-white':
