b8ef61755faadddfb0310d8cc21fa866f15b166a
Christian Escobar <christian.escobar@autocred.cl>
2022-05-05 13:21:45 -0400

    Update App, add title, description and meta tags

--- app.vue
+++ app.vue
@@ -9,4 +9,20 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
+useHead({
+  title: "Autocred",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Autocred lo hace fácil. Postula simultáneamente tu crédito con diferentes crediticias y obtén una aprobación en segundos",
+    },
+    {
+      "http-equiv": "Content-Security-Policy",
+      content: "upgrade-insecure-requests",
+    },
+  ],
+});
 </script>
