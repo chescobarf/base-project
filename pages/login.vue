d13a51093045b8c4bee8d6040ba7e4bce2b2de35
Christian Escobar <christian.escobar@autocred.cl>
2022-05-06 17:46:11 -0400

    create page login and register

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <Container>
+    <Heading1
+      content="Ingresa con tu cuenta de usuario o registrate"
+      class="text-center mb-5"
+    />
+    <Login1 />
+  </Container>
+</template>
+
+<script setup>
+import Container from "~~/layouts/Container.vue";
+import Heading1 from "~~/components/Heading.vue";
+import Login1 from "~~/components/Login.vue";
+</script>
+
+<style lang="scss" scoped></style>
