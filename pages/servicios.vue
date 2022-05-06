1e5b61f8a6dff288307afa917037faabe21a7e7f
Christian Escobar <christian.escobar@autocred.cl>
2022-05-06 13:41:33 -0400

    update servicios page

--- pages/servicios.vue
+++ pages/servicios.vue
@@ -1,46 +1,22 @@
 <template>
   <Container>
-    <BannerHome>
-      <template #content>
-        <Heading
-          content="En que consisten nuestros servicios?"
-          headingType="h2"
-          class="mb-5"
-        />
-        <Heading
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
-          headingType="h4"
-          class="mb-5"
-        />
-      </template>
-      <template #image>
-        <div class="grid place-content-center">
-          <img
-            src="https://picsum.photos/id/199/536/354"
-            alt="lorem"
-            class=""
-          />
-        </div>
-      </template>
-    </BannerHome>
-    <Servicios
-      descriptionVendedores="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates illum distinctio quasi reiciendis, magni sequi, omnis illo id ad ullam facilis fugit, obcaecati debitis iste. Blanditiis eaque amet dolorum delectus?"
-      descriptionCompradores="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates illum distinctio quasi reiciendis, magni sequi, omnis illo id ad ullam facilis fugit, obcaecati debitis iste. Blanditiis eaque amet dolorum delectus?"
-      :title="false"
+    <Heading
+      content="En que consisten nuestros servicios?"
+      headingType="h2"
+      class="mb-5"
     />
-    <Comentarios />
+    <ServiciosList1 />
     <Hablemos />
   </Container>
 </template>
 
 <script setup>
 import Container from "~~/layouts/Container.vue";
-import Servicios from "~~/layouts/Servicios.vue";
-import BannerHome from "~~/layouts/BannerHome.vue";
+
 import Heading from "~~/components/Heading.vue";
-import Button from "~~/components/Button.vue";
-import Comentarios from "~~/layouts/Comentarios.vue";
+
 import Hablemos from "~~/layouts/Hablemos.vue";
+import ServiciosList1 from "~~/layouts/ServiciosList.vue";
 </script>
 
 <style scoped></style>
