40e0c77a299311e891dfb632041175ae3b2a4e8e
Christian Escobar <christian.escobar@autocred.cl>
2022-05-09 09:50:33 -0400

    Update  Add Registro

---
+++ components/Register.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-white shadow-xl py-6 px-12 rounded-xl w-fit my-0 mx-auto">
+    <Heading1 content="Crea tu cuenta" headingType="h3" class="border-b pb-2" />
+    <div
+      class="grid items-center justify-items-center gap-4 border-b pb-4 mb-6"
+    >
+      <InputRut1 label="Rut" id="rut" class="input" />
+      <Input label="Nombre" id="nombre" class="input" />
+      <Input label="Apellidos" id="apellido" class="input" />
+      <InputEmail1 label="Correo electronico" id="email" class="input" />
+      <Input isPhone label="Telefono" id="phone" class="input" />
+      <Input password label="Contraseña" id="password" class="input" />
+      <Input password label="Repetir contraseña" id="password2" class="input" />
+
+      <Button1
+        text="Crear Cuenta"
+        secondary
+        class="justify-self-center w-fit"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Heading1 from "~~/components/Heading.vue";
+import InputRut1 from "~~/components/Input-Rut.vue";
+import Input from "./Form/Input.vue";
+import Button1 from "./Button.vue";
+import InputEmail1 from "./Input-Email.vue";
+</script>
+
+<style scoped>
+.grid .input {
+  padding-bottom: 0;
+}
+</style>
---
+++ pages/registro.vue
@@ -0,0 +1,17 @@
+<template>
+  <Container>
+    <Heading1
+      content="Ingresa con tu cuenta de usuario o registrate"
+      class="text-center mb-5"
+    />
+    <Register1 />
+  </Container>
+</template>
+
+<script setup>
+import Container from "~~/layouts/Container.vue";
+import Heading1 from "~~/components/Heading.vue";
+import Register1 from "../components/Register.vue";
+</script>
+
+<style lang="scss" scoped></style>
