02b071390f8e2aec8f7d30be6963e6ca1149c7c3
Christian Escobar <christian.escobar@autocred.cl>
2022-05-09 13:51:30 -0400

    Fix Experiencia, adding Title

--- layouts/Experiencia.vue
+++ layouts/Experiencia.vue
@@ -1,13 +1,18 @@
 <template>
+  <Heading1
+    content="Descubre la experiencia de nuestro ultimo piloto"
+    class="mb-14"
+  />
   <div class="grid gap-12 justify-between mb-24">
     <CardExperiencia1 />
-    <Video src="/assets/video/lorem.mp4" height="100%" controls />
+    <Video src="/assets/video/lorem.mp4" height="100%" controls class="pl-20" />
   </div>
 </template>
 
 <script setup>
 import Video from "~~/components/Video.vue";
 import CardExperiencia1 from "~~/components/CardExperiencia.vue";
+import Heading1 from "~~/components/Heading.vue";
 </script>
 
 <style scoped>
