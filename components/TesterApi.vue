9a98358784394e60f1f45e752ccbcc4e1c6a7eca
Christian Escobar <christian.escobar@autocred.cl>
2022-05-09 16:39:09 -0400

    Test Fetch, Axios, useFetch

--- components/TesterApi.vue
+++ components/TesterApi.vue
@@ -3,20 +3,49 @@
     <!-- <div v-for="(marca, index) in marcas.data" :key="index">
       <p>{{ marca }}</p>
     </div> -->
-    <p v-if="marca">Marca ID:</p>
+    <!-- <p v-if="marca">Marca ID:</p>
 
     <p v-else>No hay nada</p>
-    <p>{{ modelos }}</p>
+    <p>{{ modelos }}</p> -->
     <!-- <p>{{ data.data }}</p> -->
     <!-- <button @click="changeState">Click</button> -->
+    <div class="grid gap-8 mb-6 bg-black-25">
+      <h1>useFetch NUXT</h1>
+      <span>{{ marcas.data }} </span>
+      <span>{{ anios.data }}</span>
+      <span>{{ servicios.data }}</span>
+    </div>
+    <div class="grid gap-8 mb-6 bg-black-25">
+      <h1>Fetch JS</h1>
+      <span>{{ marcasFetch.data }} </span>
+    </div>
+    <div class="grid gap-8 mb-6 bg-black-25">
+      <h1>AXIOS</h1>
+      <span>{{ marcasAxios.data }} </span>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { onUpdated, ref } from "vue";
+import axios from "axios";
+import { onUpdated, ref, onMounted } from "vue";
+
+const marcas = await useFetch(
+  "http://dev.autocred.cl/api/utilities/vehicles/brands"
+);
+const anios = await useFetch(
+  "http://dev.autocred.cl/api/utilities/others/years"
+);
+const servicios = await useFetch(
+  "http://dev.autocred.cl/api/utilities/others/services"
+);
 
 const modelos = ref(0);
 const marca = useMarca();
+
+//fetch
+const marcasFetch = ref([]);
+const marcasAxios = ref([]);
 
 onUpdated(async () => {
   try {
@@ -28,6 +57,32 @@ onUpdated(async () => {
     console.log(error);
   }
 });
+
+onMounted(async () => {
+  try {
+    const res = await fetch(
+      "http://dev.autocred.cl/api/utilities/vehicles/brands"
+    );
+    marcasFetch.value = await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const axiosGet = () => {
+  axios
+    .get("http://dev.autocred.cl/api/utilities/vehicles/brands")
+    .then(function (response) {
+      // handle success
+      marcasAxios.value = response;
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+};
+axiosGet();
 </script>
 
 <style lang="scss" scoped></style>
---
+++ pages/testing.vue
@@ -0,0 +1,12 @@
+<template>
+  <Container>
+    <TesterApi />
+  </Container>
+</template>
+
+<script setup>
+import TesterApi from "~~/components/TesterApi.vue";
+import Container from "~~/layouts/Container.vue";
+</script>
+
+<style lang="scss" scoped></style>
