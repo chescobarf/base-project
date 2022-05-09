bb015ec67328e8f793595175710c5d01580bb0cd
Christian Escobar <christian.escobar@autocred.cl>
2022-05-09 13:49:24 -0400

    Fix Button size font

--- components/Button.vue
+++ components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="whitespace-nowrap inline-flex gap-1 items-center justify-center py-2 border border-transparent rounded-lg shadow-sm text-lg px-5 cursor-pointer"
+    class="whitespace-nowrap inline-flex gap-1 items-center justify-center py-2 border border-transparent rounded-lg shadow-sm text-base px-5 cursor-pointer font-medium"
     :class="{
       'bg-secondary-900 hover:bg-secondary-700 text-white': secondary,
       'bg-primary-900 hover:bg-primary-700 text-white': primary,
@@ -8,10 +8,10 @@
         outlinePrimary,
       'bg-transparent text-secondary-900 border-2 border-secondary-900 hover:bg-secondary-900 hover:text-white':
         outlineSecondary,
-      'py-1': user,
+      'py-2': user,
     }"
   >
-    <button class="font-medium" :class="{ 'text-lg': user }">
+    <button class="font-medium" :class="{ 'text-sm': user }">
       {{ text }}
     </button>
     <UserIcon class="h-5 w-5" v-if="user" />
